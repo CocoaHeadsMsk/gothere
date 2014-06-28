d1a45b82cce7ea28662e7ffe1b392a4d13c9436d
Nikolay Morev <kolyuchiy@gmail.com>
2014-06-28 22:21:11 +0400

    Разворачивание optional в одну строку

--- gothere/gothere/Point.swift
+++ gothere/gothere/Point.swift
@@ -5,27 +5,25 @@ import Foundation
 class Point: _Point {
 
     class func pointFromJSONObject(JSONObject: AnyObject, inout error: NSError?) -> Point? {
-        if let rootObject = JSONObject as? NSDictionary {
-            if let CheckPoint = rootObject["CheckPoint"] as? NSDictionary {
-                let point = Point(managedObjectContext: GTManagedObjectContext.mainContext)
+        if let pointObject = (JSONObject as? NSDictionary)?["CheckPoint"] as? NSDictionary {
+            let point = Point(managedObjectContext: GTManagedObjectContext.mainContext)
 
-                if let url = CheckPoint["url"] as? String {
-                    point.pointURL = url
-                }
-                if let StoryId = CheckPoint["StoryId"] as? String {
-                    point.storyID = StoryId
-                }
-                if let GeoPoint = CheckPoint["GeoPoint"] as? NSDictionary {
-                    if let lat = GeoPoint["lat"] as? NSNumber {
-                        point.latitude = lat
-                    }
-                    if let lon = GeoPoint["lon"] as? NSNumber {
-                        point.longitude = lon
-                    }
-                }
-
-                return point
+            if let url = pointObject["url"] as? String {
+                point.pointURL = url
             }
+            if let StoryId = pointObject["StoryId"] as? String {
+                point.storyID = StoryId
+            }
+            if let GeoPoint = pointObject["GeoPoint"] as? NSDictionary {
+                if let lat = GeoPoint["lat"] as? NSNumber {
+                    point.latitude = lat
+                }
+                if let lon = GeoPoint["lon"] as? NSNumber {
+                    point.longitude = lon
+                }
+            }
+
+            return point
         }
 
         return nil
